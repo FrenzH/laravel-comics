a32a08f29d1912d9dd1349f16eaabe886ff73959
FrenzH <franciattom99@gmail.com>
2022-11-04 09:23:53 +0000

    get comics in dom + scss

---
+++ resources/views/home.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts._HtmlMain')
+
+
+
+@section('title')
+    DC Comics HomePage
+@endsection
+
+@section('main')
+    <div class="bg-black">
+        <h2>
+            Current Series
+        </h2>
+        <div class="container">
+            <div class="card-container">
+                @foreach ($comics as $comic)
+                    <div>
+                        @include('partials._comic-card')
+                    </div>
+                @endforeach
+            </div>
+        </div>
+
+    </div>
+
+
+    <div class="bg-blue"></div>
+@endsection
---
+++ resources/views/partials/_header.blade.php
@@ -0,0 +1,8 @@
+<header>
+    <div class="blue-bg container">
+
+    </div>
+    <div class="container">
+        <img src="{{ asset('images/dc-logo.png') }}" alt="Logo">
+    </div>
+</header>
